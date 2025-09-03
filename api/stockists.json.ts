eb0a08eee0ecb82c96168fd4bf134f548627db03
LukeCreativeInd <71154774+LukeCreativeInd@users.noreply.github.com>
2025-09-03 10:00:52 +1000

    Update stockists.json.ts

--- api/stockists.json.ts
+++ api/stockists.json.ts
@@ -20,3 +20,4 @@ export default async function handler(_req: VercelRequest, res: VercelResponse) 
   res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate'); // edge cache 5 min
   return res.status(200).json(data ?? []);
 }
+ 
